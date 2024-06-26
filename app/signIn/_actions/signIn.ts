fc2fabe35f88b7baf25315432c13cdf4f5ee4431
josuerx12 <josueaze12@gmail.com>
2024-06-26 17:44:18 -0300

    chore: action adjustments in signIn to remove next redirect error, with bugs. (in Progress)

---
+++ app/signIn/_actions/signIn.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { signIn } from "@/auth";
+import { db } from "@/providers/db";
+import { signInUserSchema } from "@/schemas/auth-schema";
+import { compare } from "bcryptjs";
+import { redirect } from "next/navigation";
+
+export async function signInAction(
+  prevState: any,
+  form: FormData
+): Promise<any> {
+  try {
+    const result = signInUserSchema.safeParse({
+      email: form.get("email"),
+      password: form.get("password"),
+    });
+
+    if (result.error) {
+      throw result.error.format();
+    }
+
+    const userFromDB = await db.user.findUnique({
+      where: {
+        email: result.data?.email,
+      },
+    });
+
+    if (!userFromDB) {
+      throw { generic: "Credenciais invalidas!" };
+    }
+
+    const verifiedUser = await compare(
+      result.data.password,
+      userFromDB.password
+    );
+
+    if (!verifiedUser) {
+      throw { generic: "Credenciais invalidas!" };
+    }
+
+    await signIn("credentials", {
+      email: result.data.email,
+      redirect: false,
+    });
+  } catch (error) {
+    return error;
+  }
+}
