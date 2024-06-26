6615ad750f8cb79a2288c21553f6784faecfd7a1
josuerx12 <josueaze12@gmail.com>
2024-06-26 17:43:35 -0300

    feat: new alters in posts page.

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,35 +1,23 @@
-import Button from "@/components/button";
 import { db } from "@/providers/db";
-import { Filter, Plus } from "lucide-react";
 import React from "react";
+import ButtonActions from "./_components/buttonActions";
+import PostCard from "@/components/postCard";
 
 const Posts = async () => {
   const posts = await db.posts.findMany();
 
   return (
     <section className="w-10/12 mx-auto">
-      <div className="flex justify-end mt-10 gap-2">
-        <Button variant="primary">
-          <div className="flex gap-2 items-center">
-            <Filter size={20} /> <span>Filtrar</span>
-          </div>
-        </Button>
-        <Button variant="secondary">
-          <div className="flex gap-2 items-center">
-            <Plus size={20} /> <span>Criar</span>
-          </div>
-        </Button>
+      <ButtonActions />
+      <div className="flex flex-col gap-6 my-5">
+        {posts.length > 0 ? (
+          posts.map((post) => <PostCard key={post.id} post={post} />)
+        ) : (
+          <p className="w-full text-center mt-4">
+            Nenhum post cadastrado ainda!
+          </p>
+        )}
       </div>
-      {posts.length > 0 ? (
-        posts.map((post) => (
-          <div key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-          </div>
-        ))
-      ) : (
-        <p className="w-full text-center mt-4">Nenhum post cadastrado ainda!</p>
-      )}
     </section>
   );
 };
